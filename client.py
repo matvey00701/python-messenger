aebdc12dd9f2bfc63f1f8bb73e566d6cbb10535f
matvey00701 <89130564356z@gmail.com>
2024-05-14 21:34:27 +0700

    basically working messenger

--- client.py
+++ client.py
@@ -1,8 +1,7 @@
 import requests
-import sys
-import os
 from tkinter import *
 from tkinter import ttk, messagebox
+import threading
 
 # server_url = 'http://127.0.0.1:5000'
 
@@ -10,67 +9,90 @@ from tkinter import ttk, messagebox
 class Cli:
 
     def __init__(self):
-        self.commands = {
-            '/login': self.login,
-            '/exit': self.exit_app,
-            '/open-chat': self.open_chat,
-            '/status': self.print_status
-        }
+
+        self.chat_list = []
+        self.lastSN = 0
 
         self.status = {
-            'chat': 'dev2-dev_account',
+            'chat': '',
             'sender': '',
-            'server_url': 'http://127.0.0.1:5000'
+            'server_url': 'http://127.0.0.1:5000',
+            'ws_url': 'ws://127.0.0.1:5000'
         }
 
-    
-    def login(self):
-        nickname = input('Enter your nickname: ')
-        self.status['sender'] = str(nickname)
-
-    def exit_app(self):
-        sys.exit()
-
-    def open_chat(self):
-        os.system('cls')
-        print('TODO: open covos\n')
-
-    def parseCommand(self, command):
-        if command in self.commands:
-            self.commands[command]()
-        else:
-            print("Wrong command :/")
-
-    def print_status(self):
-        os.system('cls')
-        print(self.status)
-
-    def send(self, message: str, chat: str, user: str):
-        data = {
-            'content': message,
-            'conversation': chat,
-            'sender': user
-        }
-    
+    def send_request(self, route: str, data: dict):
         try:
             response = requests.post(
-                self.status['server_url'] + '/send',
+                self.status['server_url'] + route,
                 json=data
             )
+            # print(response.json())
+            return response.json()
         except:
-            print("Error, unable to send a message :(")
+            messagebox.showerror('Error', f'Something went wrong... :/\nCode: {response.status_code}')
 
-    def engine(self):
-        # while True:
-        #     usr_input = input()
-        #     if usr_input[0] == '/':
-        #         self.parseCommand(str(usr_input))
-        #     else:
-        #         if self.status['chat'] == '' or self.status['sender'] == '':
-        #             print("you're not loged in or didn't chose chat")
-        #         else:
-        #             self.send(str(usr_input), self.status['chat'], self.status['sender'])
-        pass
+    
+    def on_message(message):
+        # Handle received message
+        print("Received:", message)
+
+    def on_error(error):
+        print(error)
+
+
+    def is_user(self, name):
+        data = {'name': name}
+        return self.send_request('/is_user', data)['result']
+
+
+    def login(self, name):
+        if cli.is_user(name):
+                cli.status['sender'] = name
+                return True
+        else:
+            messagebox.showerror('Error', 'No such user.')
+            return False
+
+
+    def get_chats(self):
+        data = {'name': self.status['sender']}
+        return self.send_request('/get_chats', data)['result']
+
+
+    def emit_text(self, text, string):
+        text.configure(state=NORMAL)
+        text.insert('end', string)
+        text.configure(state=DISABLED)
+
+
+    def print_messages(self, text, messages: list):
+        for message in reversed(messages):
+            date = message['date']
+            message_string = '─'*40+'\n'
+            if message['sender'] == self.status['sender']:
+                message_string += f' You                 │ {str(date)}\n'
+            else:
+                message_string += f' {message['sender']:19} │ {str(date)}\n'
+            message_string += ' ' + message['content'] + '\n\n'
+
+            self.emit_text(text, message_string)
+        text.see("end")
+
+
+    def get_messages(self):
+        data = {'conversation': self.status['chat']}
+        return self.send_request('/get_msgs', data)['result']
+        
+
+    def send(self, message: str, textbox):
+        data = {
+            'content': message,
+            'conversation': self.status['chat'],
+            'sender': self.status['sender'],
+            'sn': self.lastSN + 1
+        }
+        _ = self.send_request('/send', data)['result']
+        self.print_messages(textbox, self.get_messages())
 
 
 if __name__ == '__main__':
@@ -85,7 +107,7 @@ URL: {cli.status['server_url']}")
     def connect_window():
         def set_addr():
             cli.status['server_url'] = entry.get()
-        cnw = Tk()
+        cnw = Toplevel()
         cnw.title('Connect')
         # cnw.geometry('200x100')
         label = ttk.Label(cnw, text='Enter server address: ')
@@ -98,31 +120,39 @@ URL: {cli.status['server_url']}")
 
     def login():
         def set_user():
-            cli.status['sender'] = entry.get()
-
+            if cli.login(entry.get()):
+                cli.get_chats()
+                chat_name_box.config(state="normal")
+                chat_name_box['values'] = cli.chat_list
+                chat_name_box.config(state="readonly")
+                lgw.destroy()
 
         lgw = Tk()
         lgw.title('Log in')
-        # lgw.geometry('200x100')
         label = ttk.Label(lgw, text='Enter your username: ')
         label.grid(row=0, column=0, pady=10, padx=10)
         entry = ttk.Entry(lgw)
         entry.grid(row=0, column=1, pady=10, padx=10)
         con_btn = ttk.Button(lgw, text='Set user', command=set_user)
         con_btn.grid(row=1, column=1, pady=10, padx=10)
+        lgw.resizable(0, 0)
 
     
-    def insert():
+    def send():
         txt = message_entry.get("1.0",END)
         message_entry.delete('1.0', END)
-        chat_field.configure(state=NORMAL)
-        chat_field.insert('end', txt)
-        chat_field.configure(state=DISABLED)
+        cli.send(txt, chat_field)
+
+    def open_chat():
+        cli.status['chat'] = chat_name_box.get()
+        chat_field.config(state="normal")
+        chat_field.delete('1.0', END)
+        chat_field.config(state="disabled")
+        cli.print_messages(chat_field, cli.get_messages())
 
 
     root = Tk()
     root.title('Best messenger ever :D')
-    # root.geometry("307x437")
     root.option_add("*tearOff", FALSE)
 
 
@@ -133,30 +163,24 @@ URL: {cli.status['server_url']}")
     tools_menu.add_command(label="Log in", command=login)
     main_menu.add_cascade(label='tools', menu=tools_menu)
 
+    open_chat_frame = ttk.LabelFrame(text='Open chat', borderwidth=1, relief=GROOVE, padding=[8, 10])
 
-    chats = ['dev2-dev_account', 'Me-You', 'Aris-Momoi']
+    open_chat_frame.columnconfigure(index=0, weight=3)
+    open_chat_frame.columnconfigure(index=1, weight=1)
 
-    open_chat_frame = ttk.Frame(borderwidth=1, relief=GROOVE, padding=[8, 10])
-
-    for c in range(2): open_chat_frame.columnconfigure(index=c, weight=1)
-
-    Label(open_chat_frame, text='Open chat').grid(row=0, column=0)
-    chat_name_box = ttk.Combobox(open_chat_frame, values=chats)
-    chat_name_box.grid(row=0, column=1)
-    Button(open_chat_frame, text='Open').grid(row=0, column=2)
-
+    chat_name_box = ttk.Combobox(open_chat_frame, values=cli.chat_list)
+    chat_name_box.grid(row=0, column=0, sticky='ew')
+    Button(open_chat_frame, text='Open', command=open_chat).grid(row=0, column=1)
+    chat_name_box.config(state="readonly")
     open_chat_frame.pack(anchor=NW, fill=X, padx=5, pady=5)
 
 
-    # label = Label(text='TODO: Make the rest of the app...')
-    # label.pack()
-
-    chat_frame = ttk.Frame(borderwidth=1, relief=GROOVE, padding=[8, 10])
+    chat_frame = ttk.LabelFrame(text='Chat', borderwidth=1, relief=GROOVE, padding=[8, 10])
 
 
     text_frame = ttk.Frame(chat_frame)
 
-    chat_field = Text(text_frame, width=40, state=DISABLED)
+    chat_field = Text(text_frame, width=40, state=DISABLED, wrap='word')
     chat_field.pack()
 
     text_frame.pack()
@@ -167,7 +191,7 @@ URL: {cli.status['server_url']}")
     message_entry = Text(entry_frame, width=35, height=3)
     message_entry.grid(row=0, column=0, padx=5, pady=5)
 
-    send_btn = Button(entry_frame, text='🢅', command=insert)
+    send_btn = Button(entry_frame, text='🢅', command=send)
     send_btn.grid(row=0, column=1, padx=5, pady=5, sticky=NSEW)
 
     entry_frame.pack()
--- server.py
+++ server.py
@@ -1,7 +1,5 @@
 # In dev!
 
-import time
-from datetime import datetime
 import firebase_admin
 from firebase_admin import credentials
 from firebase_admin import firestore
@@ -16,7 +14,67 @@ db = firestore.client()
 
 @app.route("/")
 def hello():
-    return "Hello, World!"
+    return "What the hella you doin' here??"
+
+
+@app.route("/is_user", methods=['POST'])
+def is_user():
+    name = request.get_json()['name']
+    usr = db.collection('users').document(str(name)).get()
+    if usr.exists:
+        return jsonify({'result': True})
+    else:
+        return jsonify({'result': False})
+    
+
+@app.route("/get_chats", methods=['POST'])
+def get_chats():
+    name = request.get_json()['name']
+
+    chats = db.collection('conversations').where('members', 'array_contains', str(name)).get()
+
+    for i, chat in enumerate(chats):
+        chats[i] = chat.id
+
+    return jsonify({'result': chats})
+
+
+def jsonify_messages(msg_lst: list):
+    lst = []
+    for msg in msg_lst:
+        msg_content = msg.get('content')
+        msg_sender = msg.get('sender')
+        msg_date = msg.get('timestamp').strftime("%d.%m.%Y %H:%M")
+        msg_sn = msg.get('sn')
+
+        lst.append(
+            {
+                'content': msg_content,
+                'sender': msg_sender,
+                'date': msg_date,
+                'sn': msg_sn
+            }
+        )
+    return jsonify({'result': lst})
+    
+
+
+@app.route("/get_msgs", methods=['POST'])
+def get_msgs():
+    conv = request.get_json()['conversation']
+    msgs = db.collection('messages')\
+        .where('conversation_id', '==', str(conv))\
+        .order_by('sn', direction=firestore.Query.DESCENDING)\
+        .limit(50)\
+        .get()
+
+    return jsonify_messages(msgs)
+
+
+# def send_update(client_id, message_data):
+#     if client_id in client_connections:
+#         connection = client_connections[client_id]
+#         connection.send(message_data)
 
 
 @app.route("/send", methods=['POST'])
@@ -26,20 +84,28 @@ def send():
     content = data.get('content')
     convID = data.get('conversation')
     sender_name = data.get('sender')
+    sn = data.get('sn')
     timestamp = firestore.SERVER_TIMESTAMP
 
     if not content or not convID or not sender_name:
         return abort(400)
-    
-    # usr = db.collection('users').where('nickname', '==', str(sender_name)).get()
-    print(content + '; ' + convID + '; ' + sender_name)
-    
+
     message_data = {
         "content": content,
         "conversation_id": convID,
+        "sn": sn,
         "sender": str(sender_name),
         "timestamp": timestamp
     }
+
+    members = db.collection('conversations').document(convID).get().to_dict().get('members')
+    for member in members:
+        if member != sender_name:
+            # send_update(member, message_data)
+            pass
+        else:
+            pass
+
     try:
         # Add message data to Firestore
         db.collection("messages").add(message_data)
